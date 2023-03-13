8f8347c642019e3ccd56377ad0a8a08778c416e1
Elham Majd <58331755+elhammajd@users.noreply.github.com>
2023-03-13 10:32:50 -0700

    Update Parallel-jobs.sh

--- sh/Parallel-jobs.sh
+++ sh/Parallel-jobs.sh
@@ -1,30 +1,5 @@
 
-Rscript ./code/Delete-folders.R
-
-sbatch ./sh/6CV.sh 
-sbatch ./sh/7CV.sh
-sbatch ./sh/8CV.sh
-sbatch ./sh/9CV.sh
-sbatch ./sh/11CV.sh
-sbatch ./sh/12CV.sh
-sbatch ./sh/16CV.sh
-sbatch ./sh/17CV.sh
-sbatch ./sh/18CV.sh
-sbatch ./sh/22CV.sh
-sbatch ./sh/23CV.sh
-sbatch ./sh/31CV.sh
-sbatch ./sh/32CV.sh
-sbatch ./sh/34CV.sh
-sbatch ./sh/35CV.sh
-sbatch ./sh/37CV.sh
-sbatch ./sh/41CV.sh
-sbatch ./sh/42CV.sh
-sbatch ./sh/53CV.sh 
-sbatch ./sh/54CV.sh 
-sbatch ./sh/55CV.sh
-sbatch ./sh/58CV.sh
-sbatch ./sh/59CV.sh
-
+sbatch ./sh/CV.sh 
 
 
 
