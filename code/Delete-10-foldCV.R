443d79c5872e300093c11dd0bf044799460b6b5c
Elham Majd <elh.majd@gmail.com>
2023-03-13 10:14:50 -0700

    scripts

--- code/Delete-10-foldCV.R
+++
@@ -1,1 +0,0 @@
- unlink(c("./cvresults/*","./coef_v3/*", "./coef_v4/*", "./val_v3/*", "./val_v4/*"))
--- code/Delete-Rscripts.R
+++
@@ -1,3 +0,0 @@
-  unlink(c("./Resultmethod1/*", "./rate/*", "./read/*", "./readtree/*", "./Osmethod1/*", "./Osmethod2/*", "./Osmethod3/*", "./finalresultmethod2/*", 
-  "./finalresultmethod3/*", "./finalmethod2/*", "./finalmethod3/*", "./scoremethod1/*", "./scoremethod2/*", "./scoremethod3/*", 
-  "./csvlist/*", "./csvlist2/*", "./Resultmethod2/*", "./Resultmethod3/*", "./coef_v4/*", "./coef_v3/*", "./method2results/*", "./method3results/*"))
